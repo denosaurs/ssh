ce8abfa881e55faba5235bbeaaa7fa63b0260d67
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-07-20 19:04:58 -0400

    feat: docs

--- Client.ts
+++ Client.ts
@@ -1,3 +1,44 @@
+// deno-lint-ignore-file no-unused-vars
+
+/**
+ * The options for the client.
+ */
+export interface ClientOptions {
+  /**
+   * The hostname or IP address of the server.
+   */
+  hostname?: string;
+
+  /**
+   * The port number of the server.
+   */
+  port?: number;
+
+  /**
+   * The username to authenticate as.
+   */
+  username?: string;
+
+  /**
+   * The password to use for authentication.
+   */
+  password?: string;
+
+  /**
+   * The private key to use for authentication.
+   */
+  privateKey?: string;
+
+  /**
+   * The passphrase to use for decrypting the private key.
+   */
+  passphrase?: string;
+}
+
+/**
+ * The base SSH client class for connecting to the server.
+ * Each instance of this class represents a single connection to the server.
+ */
 export class Client {
   #connection: Deno.TcpConn | null;
   isConnected: boolean;
@@ -6,18 +47,14 @@ export class Client {
     hostname = "127.0.0.1",
     port = 22,
     username = "root",
-  }: {
-    hostname?: string;
-    port?: number;
-    username?: string;
-    password?: string;
-    privateKey?: string;
-    passphrase?: string;
-  } = {}) {
+  }: ClientOptions = {}) {
     this.#connection = null;
     this.isConnected = false;
   }
 
+  /**
+   * Connect to the server.
+   */
   async connect() {
     // this.#connection = await Deno.connect();
 
@@ -25,21 +62,39 @@ export class Client {
     // this.isConnected = true;
   }
 
+  /**
+   * Disconnect from the server.
+   */
   disconnect() {
   }
 
-  async getDirectory() {
+  /**
+   * Get a directory listing. If a path is given, the listing will start from that path.
+   */
+  async getDirectory(path?: string) {
   }
 
-  async getFile() {
+  /**
+   * Get a file. If a path is given, the file will be retrieved from that path.
+   */
+  async getFile(path?: string) {
   }
 
-  async putDirectory() {
+  /**
+   * Put a directory. If a path is given, the directory will be put to that path.
+   */
+  async putDirectory(path?: string) {
   }
 
-  async putFile() {
+  /**
+   * Put a file. If a path is given, the file will be put to that path.
+   */
+  async putFile(path?: string) {
   }
 
+  /**
+   * Run a command on the server.
+   */
   async run(command: string, {
     cwd,
   }: {
